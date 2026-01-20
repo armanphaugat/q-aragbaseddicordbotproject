ca406722579e5d978aeaae9aa42af1bde4853d22
Arman <armanphaugat20@gmail.com>
2026-01-20 11:58:01 +0530

    Modularity Added

--- python/ingest.py
+++ python/ingest.py
@@ -4,20 +4,39 @@ from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_community.document_loaders import WebBaseLoader
 from langchain_community.embeddings import HuggingFaceEmbeddings
 from bs4 import SoupStrainer
+import PyPDF2
+
 DB_PATH = "vectorstore/faiss_zomato"
 URL = "https://www.zomato.com/policies/terms-of-service"
 TAGS = ["h1", "h2", "h3", "p", "li"]
+
 def webscraper(url):
     loader = WebBaseLoader(url, bs_kwargs={"parse_only": SoupStrainer(TAGS)})
     return loader.load()
-docs = webscraper(URL)
-splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=200)
-splits = splitter.split_documents(docs)
-embeddings = HuggingFaceEmbeddings(
-    model_name="sentence-transformers/all-MiniLM-L6-v2"
-)
-vectorstore = FAISS.from_documents(splits, embeddings)
-os.makedirs("vectorstore", exist_ok=True)
-vectorstore.save_local(DB_PATH)
 
-print(f"✅ FAISS vector store saved locally at: {DB_PATH}")
+def read_pdf(file_path):
+    with open(file_path, "rb") as f:
+        reader = PyPDF2.PdfReader(f)
+        text = ""
+        for page in reader.pages:
+            text += page.extract_text() + "\n"
+    return text
+
+def combine(url, file_path):
+    docs = webscraper(url)
+    pdfs = read_pdf(file_path)
+    return docs + pdfs
+
+def split_documents(docs):
+    splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=200)
+    return splitter.split_documents(docs)
+
+def create_embeddings():
+    return HuggingFaceEmbeddings(model_name="sentence-transformers/all-MiniLM-L6-v2")
+
+def create_vectorstore(docs, embeddings):
+    vectorstore = FAISS.from_documents(docs, embeddings)
+    os.makedirs("vectorstore", exist_ok=True)
+    vectorstore.save_local(DB_PATH)
+    return vectorstore
+
